105ea7f2e2f21b470005e0b97a2abdd261a925dd
MiracleDevelopment <i.patipan2539@gmail.com>
2017-08-21 17:28:39 +0700

    Update Ripple

--- app/src/main/java/com/ipati/dev/castleevent/fragment/loading/LoadingFragment.kt
+++ app/src/main/java/com/ipati/dev/castleevent/fragment/loading/LoadingFragment.kt
@@ -9,7 +9,7 @@ import com.ipati.dev.castleevent.R
 import kotlinx.android.synthetic.main.activity_loading_fragment.*
 
 class LoadingFragment : Fragment() {
-    var bundle: Bundle? = null
+    private var bundle: Bundle? = null
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.activity_loading_fragment, container, false)
     }
@@ -19,9 +19,9 @@ class LoadingFragment : Fragment() {
         if (bundle != null) {
             val status: Boolean = bundle?.getBoolean(loadingFragmentBundle, false)!!
             if (status) {
-                loading_view.show()
+
             } else {
-                loading_view.hide()
+
             }
         }
     }
