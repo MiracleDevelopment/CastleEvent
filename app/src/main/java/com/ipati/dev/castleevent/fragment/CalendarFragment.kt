637a9aa999195b5c8c476f1c497dcd6df685a3b2
MiracleDevelopment <i.patipan2539@gmail.com>
2017-08-29 18:08:27 +0700

    Calendar Confix

--- app/src/main/java/com/ipati/dev/castleevent/fragment/CalendarFragment.kt
+++ app/src/main/java/com/ipati/dev/castleevent/fragment/CalendarFragment.kt
@@ -1,15 +1,24 @@
 package com.ipati.dev.castleevent.fragment
 
-import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.sundeepk.compactcalendarview.CompactCalendarView
+import com.github.sundeepk.compactcalendarview.domain.Event
 import com.ipati.dev.castleevent.R
-
+import kotlinx.android.synthetic.main.activity_calendar_fragment.*
+import java.util.*
 
 class CalendarFragment : Fragment(), View.OnClickListener {
+    var mCalendar: Calendar = Calendar.getInstance()
+    lateinit var monthDefault: String
+    lateinit var monthScroll: String
+    lateinit var dateScroll: String
+    lateinit var event: Event
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -20,6 +29,42 @@ class CalendarFragment : Fragment(), View.OnClickListener {
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initialCalendar()
+        defaultMonth()
+
+        tv_header_month.text = defaultMonth()
+        addEvent()
+    }
+
+    private fun defaultMonth(): String {
+        monthDefault = mCalendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault())
+        return monthDefault
+    }
+
+    private fun initialCalendar() {
+        compat_calendar_view.displayOtherMonthDays(true)
+        compat_calendar_view.setFirstDayOfWeek(Calendar.WEDNESDAY)
+        compat_calendar_view.setListener(object : CompactCalendarView.CompactCalendarViewListener {
+            override fun onDayClick(dateClicked: Date?) {
+                mCalendar.time = dateClicked
+                tv_calendar_select_date.text = mCalendar.get(Calendar.DATE).toString()
+            }
+
+            override fun onMonthScroll(firstDayOfNewMonth: Date?) {
+                mCalendar.time = firstDayOfNewMonth
+                monthScroll = mCalendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault())
+                dateScroll = mCalendar.get(Calendar.DATE).toString()
+                tv_header_month.text = monthScroll
+                tv_calendar_select_date.text = dateScroll
+            }
+        })
+    }
+
+    private fun addEvent() {
+        mCalendar.timeZone = TimeZone.getDefault()
+        mCalendar.set(Calendar.YEAR, Calendar.MONTH, Calendar.DATE, Calendar.HOUR_OF_DAY, Calendar.MINUTE, Calendar.SECOND)
+        event = Event(ContextCompat.getColor(context, R.color.colorEvent), mCalendar.timeInMillis, "Hello Event")
+        compat_calendar_view.addEvent(event, true)
 
     }
 
